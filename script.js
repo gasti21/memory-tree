8f27692ff88cf0983dcb11c46049ec42ae89fe5e
gasti21 <124746034+gasti21@users.noreply.github.com>
2026-06-21 17:28:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -113,10 +113,10 @@ function getBirthdayStatus() {
     target = new Date(year + 1, 5, 22, 0, 0, 0);
   }
 
-  return {
-    isBirthday: false,
-    diff: target - now
-  };
+return {
+  isBirthday: true,
+  diff: 0
+};
 }
 
 function applyBirthdayState() {
